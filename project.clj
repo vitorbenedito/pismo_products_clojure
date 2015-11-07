29fb4bd2ec7cbf8d3de18e71f244c2a1137f99b6
vitorbenedito <vitor.benedito@gmail.com>
2015-11-07 00:34:39 -0200

    heroku

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject api-test "0.1.0-SNAPSHOT"
+(defproject products "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -8,6 +8,9 @@
                  [org.clojure/data.xml "0.0.8"]
                  [compojure "1.1.8"]]
   :plugins [[lein-ring "0.8.11"]]
+  :main products.service
+  :min-lein-version "2.0.0"
+  :uberjar-name "product-app.jar"
   :ring {:handler products.service/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
